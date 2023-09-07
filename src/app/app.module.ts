63ea081d797f9f52d64b6abd7f3b7c977a6ab707
Chris Gorham <chris.gorham@outlook.com>
2023-09-07 07:16:58 -0400

    attribution

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
-/**
- * Title: app.module.ts
- * Author: Professor Krasso
- * Date: 8/5/23
- */
+/*
+======================================
+; Title: app.module.ts
+; Author: Chris Gorham, Shane Hingtgen
+; Date Created: 07 September 2023
+; Last Updated: 07 September 2023
+; Description: This code supports the App Module and handles all imports
+; Sources Used: Bellevue University WEB-450 GitHub Repository
+;=====================================
+*/
 
 // imports statements
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
 import { BaseLayoutComponent } from './layouts/base-layout/base-layout.component';
-import { NavComponent } from './layouts/nav/nav.component';
 import { FooterComponent } from './layouts/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './layouts/nav/nav.component';
+import { NgModule } from '@angular/core';
+
 
 @NgModule({
   declarations: [
