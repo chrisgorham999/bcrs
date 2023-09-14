4c4118a023bf0ea8925f91507ed625f0f9ee6d4b
Chris Gorham <chris.gorham@outlook.com>
2023-09-14 16:06:35 -0400

    sign in and config updates

--- src/app/layouts/nav/nav.component.ts
+++ src/app/layouts/nav/nav.component.ts
@@ -1,6 +1,6 @@
 /*
 ======================================
-; Title: home.component.ts
+; Title: nav.component.ts
 ; Author: Chris Gorham, Shane Hingtgen
 ; Date Created: 07 September 2023
 ; Last Updated: 07 September 2023
@@ -11,6 +11,13 @@
 
 // imports
 import { Component } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+
+export interface SessionUser {
+  fullName: string
+  role: string
+}
+
 
 @Component({
   selector: 'app-nav',
@@ -18,5 +25,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
+  sessionUser: SessionUser
+  isSignedIn: boolean
+
+  constructor(private cookieService: CookieService) {
+    this.sessionUser = {} as SessionUser
+    this.isSignedIn = this.cookieService.get('session_user') ? true: false
+
+    if (this.isSignedIn) {
+      this.sessionUser = JSON.parse(this.cookieService.get('session_user'))
+      console.log('Session User:', this.sessionUser)
+    }
+  }
+
+  signout() {
+    this.cookieService.deleteAll();
+    window.location.href ='/'
+  }
 
 }
