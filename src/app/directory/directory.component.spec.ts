5579add51066035d3ec45d3e2d23eee677b2368a
Chris Gorham <chris.gorham@outlook.com>
2023-09-22 14:43:02 -0400

    image optimizations

--- src/app/directory/directory.component.spec.ts
+++ src/app/directory/directory.component.spec.ts
@@ -1,5 +1,16 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+/*
+======================================
+; Title: directory.component.spec.ts
+; Author: Chris Gorham, Shane Hingtgen
+; Date Created: 22 September 2023
+; Last Updated: 22 September 2023
+; Description: This code supports the Directory Component
+; Sources Used: Bellevue University WEB-450 GitHub Repository
+;=====================================
+*/
 
+// imports statements
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DirectoryComponent } from './directory.component';
 
 describe('DirectoryComponent', () => {
--- src/app/directory/directory.component.ts
+++ src/app/directory/directory.component.ts
@@ -1,3 +1,15 @@
+/*
+======================================
+; Title: directory.component.ts
+; Author: Chris Gorham, Shane Hingtgen
+; Date Created: 22 September 2023
+; Last Updated: 22 September 2023
+; Description: This code supports the Directory Component
+; Sources Used: Bellevue University WEB-450 GitHub Repository
+;=====================================
+*/
+
+// imports statements
 import { Component } from '@angular/core';
 
 @Component({
